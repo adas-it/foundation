72b5798008cf94d87c6dd08c3f70c362ea3d7544
Oliveira Lucas <lucas.oliveira5@pt.bosch.com>
2025-10-22 17:52:09 +0100

    Added Foundation.Infrastructure  - ExpressionExtensions

---
+++ src/Foundation.Infrastructure/AdasIt.Foundation.Infrastructure.EntityFramework/ExpressionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+
+namespace AdasIt.Foundation.Infrastructure.EntityFramework;
+
+public static class ExpressionExtensions
+{
+    public static Expression<Func<T, bool>> Or<T>(
+        this Expression<Func<T, bool>> left,
+        Expression<Func<T, bool>> right)
+    {
+        var parameter = Expression.Parameter(typeof(T), "x");
+
+        var leftVisitor = new ReplaceParameterVisitor(left.Parameters[0], parameter);
+        var leftBody = leftVisitor.Visit(left.Body);
+
+        var rightVisitor = new ReplaceParameterVisitor(right.Parameters[0], parameter);
+        var rightBody = rightVisitor.Visit(right.Body);
+
+        var body = Expression.OrElse(leftBody, rightBody);
+        return Expression.Lambda<Func<T, bool>>(body, parameter);
+    }
+
+    private class ReplaceParameterVisitor : ExpressionVisitor
+    {
+        private readonly ParameterExpression _oldParameter;
+        private readonly ParameterExpression _newParameter;
+
+        public ReplaceParameterVisitor(ParameterExpression oldParameter, ParameterExpression newParameter)
+        {
+            _oldParameter = oldParameter;
+            _newParameter = newParameter;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return node == _oldParameter ? _newParameter : base.VisitParameter(node);
+        }
+    }
+}
